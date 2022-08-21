7035a8196dd1a988d1e85163772f6ea617f186b7
Agustín Alejandro dos Santos <dossan.92@gmail.com>
2022-08-21 09:12:31 -0300

    Initial backend rest api routes

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/upeim/api/get-all-employees", (req, res) => {
+  res.send("json de empleados");
+});
+
+app.post("/upeim/api/add-employee", (req, res) => {});
+
+app.delete("/upeim/api/remove-employee", (req, res) => {});
+
+app.put("/upeim/api/edit-employee", (req, res) => {});
+
+app.listen(1337, () => {
+  console.log("Iniciando servidor en puerto 1337...");
+});
