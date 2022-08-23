46b993ab2e0918f06802baae2c75b152f3f0a031
Agustín Alejandro dos Santos <dossan.92@gmail.com>
2022-08-23 15:02:49 -0300

    Implemented edit-employee endpoint

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,31 @@ app.post("/upeim/api/add-employee", (req, res) => {
 
 app.delete("/upeim/api/remove-employee", (req, res) => {});
 
-app.put("/upeim/api/edit-employee", (req, res) => {});
+app.put("/upeim/api/edit-employee", async (req, res) => {
+  if (
+    req.body.id == null ||
+    req.body.nombreCompleto == null ||
+    req.body.documentoIdentidad == null ||
+    req.body.fechaNacimiento == null ||
+    req.body.esDesarrollador == null ||
+    req.body.descripcion == null ||
+    req.body.areaId == null
+  ) {
+    res.status(400).json({ resultado: false });
+    return;
+  }
+  await upeimDatabase.updateEmployeesById(
+    dataBaseName,
+    res,
+    req.body.id,
+    req.body.nombreCompleto,
+    req.body.documentoIdentidad,
+    req.body.fechaNacimiento,
+    req.body.esDesarrollador.toUpperCase(),
+    req.body.descripcion,
+    req.body.areaId
+  );
+});
 
 app.listen(1337, () => {
   console.log("Iniciando servidor en puerto 1337...");
