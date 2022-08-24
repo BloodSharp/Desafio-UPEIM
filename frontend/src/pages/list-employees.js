43baa49792a6c5c8a33e005155f45cd698a3c516
Agustín Alejandro dos Santos <dossan.92@gmail.com>
2022-08-24 14:15:38 -0300

    Added GetEmployeesForm

--- frontend/src/pages/list-employees.js
+++ frontend/src/pages/list-employees.js
@@ -4,29 +4,30 @@ import LogoBA from "./components/logo-ba";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import GetEmployeesForm from "./components/get-employees";
 
 function ListEmployees() {
   return (
     <div>
       <NavigationBar />
+      <br />
       <Container>
         <Row>
           <Col />
           <Col>
             <LogoBA />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
           </Col>
           <Col />
+        </Row>
+        <Row>
+          <Col>
+            <br />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <GetEmployeesForm />
+          </Col>
         </Row>
       </Container>
     </div>
