215e3349f09e90f7af136cedf8c064e7efe1cdcc
Agustín Alejandro dos Santos <dossan.92@gmail.com>
2022-08-24 13:04:22 -0300

    Added containers, rows and columns

--- frontend/src/pages/add-employees.js
+++ frontend/src/pages/add-employees.js
@@ -1,22 +1,34 @@
 import NavigationBar from "./components/nav-bar";
 import LogoBA from "./components/logo-ba";
 
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 function AddEmployees() {
   return (
     <div>
       <NavigationBar />
-      <LogoBA />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+      <Container>
+        <Row>
+          <Col />
+          <Col>
+            <LogoBA />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </Col>
+          <Col />
+        </Row>
+      </Container>
     </div>
   );
 }
